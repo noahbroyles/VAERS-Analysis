dbe845c62aaff4ebf7d8b84758180b132a9e654b
Noah Broyles <noah@javamate.net>
2021-10-14 09:24:36 -0400

    Much faster data insertion

--- addCSVData.py
+++ addCSVData.py
@@ -5,10 +5,9 @@ import creds
 
 from addict import Dict
 from datetime import datetime
+from database import Database
 from svFileConvertor import csv2json
 from pymysql.err import ProgrammingError
-from database import Database, PreparedStatement
-
 
 
 def get_date(date_string):
@@ -19,7 +18,7 @@ def get_date(date_string):
 def determine_bool(string):
     if string == "Y":
         return 1
-    elif string == "":
+    elif string == "U" or string == "":
         return ''
     return 0
 
@@ -32,9 +31,50 @@ for csv in data_filenames:
     print(f'ADDING RECORDS FROM {csv[:4]}...')
     records = [Dict(r) for r in csv2json(f"data/{csv}")]
 
+    params = []
+    record_count = len(records)
     for record in records:
-        print(f"\tAdding record {record.VAERS_ID}")
-        pstmt = PreparedStatement("""INSERT IGNORE INTO tInjuryData (
+        print(f"\tPreparing record {record.VAERS_ID}")
+        c_params = [
+            record.VAERS_ID, 
+            get_date(record.RECVDATE),
+            record.STATE,
+            record.AGE_YRS,
+            record.CAGE_YR,
+            record.CAGE_MO,
+            record.SEX,
+            get_date(record.RPT_DATE),
+            record.SYMPTOM_TEXT,
+            determine_bool(record.DIED),
+            get_date(record.DATEDIED),
+            determine_bool(record.L_THREAT),
+            determine_bool(record.ER_VISIT), 
+            determine_bool(record.HOSPITAL),
+            record.HOSPDAYS,
+            determine_bool(record.X_STAY),
+            determine_bool(record.DISABLE),
+            determine_bool(record.RECOVD),
+            get_date(record.VAX_DATE),
+            get_date(record.ONSET_DATE),
+            record.NUMDAYS,
+            record.LAB_DATA,
+            record.V_ADMINBY,
+            record.V_FUNDBY,
+            record.OTHER_MEDS,
+            record.CUR_ILL,
+            record.HISTORY,
+            record.PRIOR_VAX,
+            record.SPLTTYPE,
+            determine_bool(record.BIRTH_DEFECT),
+            determine_bool(record.OFC_VISIT),
+            determine_bool(record.ER_ED_VISIT),
+            record.ALLERGIES
+        ]
+        params += c_params
+
+    print('Executing statement in database...')
+    try:
+        db.execute_stmt(f"""INSERT IGNORE INTO tInjuryData (
             VAERS_ID,
             ReceiveDate,
             State,
@@ -68,55 +108,21 @@ for csv in data_filenames:
             OFFICIAL_VISIT,
             ER_ED_VISIT,
             Allergies
-            ) 
+            )
             VALUES
-            (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)""",
-            params=[
-                record.VAERS_ID, 
-                get_date(record.RECVDATE),
-                record.STATE,
-                record.AGE_YRS,
-                record.CAGE_YR,
-                record.CAGE_MO,
-                record.SEX,
-                get_date(record.RPT_DATE),
-                record.SYMPTOM_TEXT,
-                determine_bool(record.DIED),
-                get_date(record.DATEDIED),
-                determine_bool(record.L_THREAT),
-                determine_bool(record.ER_VISIT), 
-                determine_bool(record.HOSPITAL),
-                record.HOSPDAYS,
-                determine_bool(record.X_STAY),
-                determine_bool(record.DISABLE),
-                determine_bool(record.RECOVD),
-                get_date(record.VAX_DATE),
-                get_date(record.ONSET_DATE),
-                record.NUMDAYS,
-                record.LAB_DATA,
-                record.V_ADMINBY,
-                record.V_FUNDBY,
-                record.OTHER_MEDS,
-                record.CUR_ILL,
-                record.HISTORY,
-                record.PRIOR_VAX,
-                record.SPLTTYPE,
-                determine_bool(record.BIRTH_DEFECT),
-                determine_bool(record.OFC_VISIT),
-                determine_bool(record.ER_ED_VISIT),
-                record.ALLERGIES
-            ], convert_blanks_to_nulls=True)
+            {",".join('(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)' for _ in range(record_count))}""",
+            params=params, convert_blanks_to_nulls=True)
+    except ProgrammingError:
+        with open('errors.json', 'r') as rf:
+            errors = json.load(rf)
+        errors.append(csv)
+        with open('errors.json', 'w') as wf:
+            json.dump(errors, wf, indent=4)
 
-        try:
-            db.execute_stmt(pstmt.get_finished_sql(), commit=True)
-        except ProgrammingError:
-            with open('errors.json', 'r') as rf:
-                errors = json.load(rf)
-            errors[record.VAERS_ID] = record
-            with open('errors.json', 'w') as wf:
-                json.dump(errors, wf, indent=4)
+    print('Done.')
+
                 
-    os.system(f"mv data/{csv} processed/{csv}")
+    os.system(f"mv /home/nbroyles/Documents/Yoshi/data/{csv} /home/nbroyles/Documents/Yoshi/processed/{csv}")
     print()
 
 db.close()
