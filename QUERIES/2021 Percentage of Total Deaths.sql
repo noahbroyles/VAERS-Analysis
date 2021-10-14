b0500fa1de1ea7239ce4b33302ec89ca647a32e4
Noah Broyles <noah@javamate.net>
2021-10-14 13:09:30 -0400

    Add query results

--- QUERIES/2021 Percentage of Total Deaths.sql
+++ QUERIES/2021 Percentage of Total Deaths.sql
@@ -2,4 +2,13 @@ SELECT ((SELECT COUNT(*) as DeathsIn2021
 FROM tInjuryData
 WHERE DIED = 1 AND YEAR(ReceiveDate) = 2021) / (SELECT COUNT(*) as AllDeathsSince1990
 FROM tInjuryData
-WHERE DIED = 1) * 100) as `Pecentage of 2021 Deaths per All Deaths`
+WHERE DIED = 1) * 100) as `2021 Deaths Percentage`
+
+/*
++----------------------+
+|2021 Deaths Percentage|
++----------------------+
+|65.4831               |
++----------------------+
+
+*/
