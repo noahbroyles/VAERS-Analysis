209ad081959f0798c280bc2b1aa28ddc14fe8192
Noah Broyles <noah@javamate.net>
2021-10-14 13:10:51 -0400

    Beautify query

--- QUERIES/2021 Percentage of Total Deaths.sql
+++ QUERIES/2021 Percentage of Total Deaths.sql
@@ -1,10 +1,10 @@
-SELECT ((SELECT COUNT(*) as DeathsIn2021
-FROM tInjuryData
-WHERE DIED = 1 AND YEAR(ReceiveDate) = 2021) / (SELECT COUNT(*) as AllDeathsSince1990
-FROM tInjuryData
-WHERE DIED = 1) * 100) as `2021 Deaths Percentage`
+SELECT ((SELECT COUNT(*) as DeathsIn2021 FROM tInjuryData WHERE DIED = 1 AND YEAR(ReceiveDate) = 2021)
+     / (SELECT COUNT(*) as AllDeathsSince1990 FROM tInjuryData WHERE DIED = 1)
+     * 100)
+AS `2021 Deaths Percentage`
 
 /*
+
 +----------------------+
 |2021 Deaths Percentage|
 +----------------------+
